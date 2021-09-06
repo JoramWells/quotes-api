98c05d0f5c564248c5a93da9c4707682c04661a3
bramuel <jorammanoah1@gmail.com>
2021-09-06 02:47:58 -0700

    Added pagination

--- app.js
+++ app.js
@@ -7,10 +7,60 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3000
-
 // console.log(data[0])
 app.get("/quotes", (req, res) => {
-  res.send(data);
+  var offset = 5
+  var pg_data = 0
+  var start_index = 0
+  var end_index = 0
+
+  const { page, items } = req.query
+  const page_number = (data_length, offset) => {
+    var no_of_pages = Math.floor(data_length / offset);
+    var remaining_items = data_length % offset;
+    if (remaining_items) res.json({"Page limit":no_of_pages + 1})
+    res.json({"Page limit":no_of_pages})
+
+  }
+
+  // Check if page has been passed as a query parameter
+  if ((page)) {
+
+    // Check if the number of items has been specified
+    if (items) {
+      offset = items
+      start_index = (items * page) - items
+      end_index = items * page
+      pg_data = data.slice(start_index, end_index)
+
+    } else {
+      // Default to an item number of 5
+      start_index = (5 * page) - 5
+      end_index = 5 * page
+      pg_data = data.slice(start_index, end_index)
+    }
+
+
+    // Check if the request has no page.
+    if (pg_data.length == 0) {
+      page_number(data.length,offset)
+    } else {
+      res.json({
+        "page number": parseInt(page),
+        "items": parseInt(offset),
+        "number of pages": data.length%offset?Math.floor(data.length/offset)+1:Math.floor(data.length),
+        "data": pg_data
+      })
+
+
+    }
+  }
+
+  // If their is no pagination, send the entire data
+  else {
+    res.send(data);
+
+  }
 });
 
 app.get("/quote/:id", (req, res) => {
@@ -23,6 +73,7 @@ app.get("/quotes/rand", (req, res) => {
   const id = Math.floor(Math.random() * data.length);
   res.send(data[id]);
 });
+
 
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
